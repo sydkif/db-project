67ac4758ed1ecfe970fdb07e554222b1bf24d0cd
Syed Akif <57567504+sydkif@users.noreply.github.com>
2021-05-15 23:30:18 +0800

    Update lecturer.php

--- admin/register/lecturer.php
+++ admin/register/lecturer.php
@@ -11,7 +11,7 @@
             <table class="table table-striped">
                 <thead class="thead-dark">
                     <tr>
-                        <th style="width: 5%;">Lecturer ID</th>
+                        <th style="width: 10%;">Lecturer ID</th>
                         <th style="width: auto;">Lecturer Name</th>
                         <th style="width: 20%;">Modified By</th>
                         <th style="width: 20%;">Modified On</th>
@@ -21,77 +21,80 @@
                 </thead>
                 <tbody>
 
-                    <tr>
-                        <th>1822</th>
-                        <td>CHUAH CHAI WEN</td>
-                        <td>FARIDAH SUPANJI</td>
-                        <td>09/05/2021 13:10:42</td>
-                        <td><button class="btn btn-sm btn-primary">Update</button></td>
-                        <td><button class="btn btn-sm btn-danger">Delete</button></td>
-                    </tr>
+                    <?php
+                    include "../../DB.php";
+
+                    $sql = "SELECT * FROM lecturer";
+                    $result = $conn->query($sql);
+                    $num = 0;
+
+                    if ($result->num_rows > 0) {
+                        // output data of each row
+                        while ($row = $result->fetch_assoc()) {
+                            ++$num;
+                    ?>
+
+                            <tr>
+                                <td id="id<?php echo $num ?>"><b><?php echo $row["id"] ?><b></td>
+                                <td contentEditable=" false" id="name<?php echo $num ?>" style="text-transform: uppercase;"><?php echo $row["name"] ?></td>
+                                <td><?php echo $row["modiBy"] ?></td>
+                                <td><?php echo $row["modiOn"]  ?></td>
+                                <td>
+                                    <button id="update<?php echo $num ?>" onclick="edit(<?php echo $num ?>)" class="btn btn-sm updateBtn">
+                                        <i class="bi bi-pencil-square" style="color: blue;"></i>
+                                    </button>
+                                </td>
+                                <td>
+                                    <button id="delete<?php echo $num ?>" class="btn btn-sm" onclick="remove(<?php echo $num ?>)">
+                                        <i class="bi bi-trash" style="color: red;"></i>
+                                    </button>
+                                </td>
+                            </tr>
+
+                    <?php
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    $conn->close();
+                    ?>
+
 
                     <tr>
-                        <th>2266</th>
-                        <td>SHAREEN KASIM</td>
-                        <td>SAMSUDIN ABDULLAH</td>
-                        <td>09/05/2021 13:11:22</td>
-                        <td><button class="btn btn-sm btn-primary">Update</button></td>
-                        <td><button class="btn btn-sm btn-danger">Delete</button></td>
+                        <form action="" method="post">
+                            <td><input style="width: 60px;" name="id" value="" type="text" minlength="4" maxlength="4" pattern="\d*" required></td>
+                            <td><input style="width: 360px;" name="name" value="" type="text" required></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <button name="add" class="btn btn-sm" href="#?">
+                                    <i class="bi bi-plus-square" style="color: green;"></i>
+                                </button>
+                            </td>
+                        </form>
                     </tr>
 
-                    <tr>
-                        <th>1822</th>
-                        <td>CHUAH CHAI WEN</td>
-                        <td>FARIDAH SUPANJI</td>
-                        <td>09/05/2021 13:10:42</td>
-                        <td><button class="btn btn-sm btn-primary">Update</button></td>
-                        <td><button class="btn btn-sm btn-danger">Delete</button></td>
-                    </tr>
+                    <?php
 
-                    <tr>
-                        <th>2266</th>
-                        <td>SHAREEN KASIM</td>
-                        <td>SAMSUDIN ABDULLAH</td>
-                        <td>09/05/2021 13:11:22</td>
-                        <td><button class="btn btn-sm btn-primary">Update</button></td>
-                        <td><button class="btn btn-sm btn-danger">Delete</button></td>
-                    </tr>
+                    include "../../DB.php";
 
-                    <tr>
-                        <th>1822</th>
-                        <td>CHUAH CHAI WEN</td>
-                        <td>FARIDAH SUPANJI</td>
-                        <td>09/05/2021 13:10:42</td>
-                        <td><button class="btn btn-sm btn-primary">Update</button></td>
-                        <td><button class="btn btn-sm btn-danger">Delete</button></td>
-                    </tr>
+                    if (isset($_POST['add'])) {
+                        $id = $_POST['id'];
+                        $name = $_POST['name'];
+                        $sql = "INSERT INTO lecturer (id, name) VALUES ('$id', '$name')";
 
-                    <tr>
-                        <th>2266</th>
-                        <td>SHAREEN KASIM</td>
-                        <td>SAMSUDIN ABDULLAH</td>
-                        <td>09/05/2021 13:11:22</td>
-                        <td><button class="btn btn-sm btn-primary">Update</button></td>
-                        <td><button class="btn btn-sm btn-danger">Delete</button></td>
-                    </tr>
+                        if ($conn->query($sql) === true) {
+                            // Success
+                            echo "<meta http-equiv='refresh' content='0'>";
+                        } else {
+                            // Failed
+                            echo "Error: " . $sql . "<br>" . $conn->error;
+                        }
+                    }
 
-                    <tr>
-                        <th>1822</th>
-                        <td>CHUAH CHAI WEN</td>
-                        <td>FARIDAH SUPANJI</td>
-                        <td>09/05/2021 13:10:42</td>
-                        <td><button class="btn btn-sm btn-primary">Update</button></td>
-                        <td><button class="btn btn-sm btn-danger">Delete</button></td>
-                    </tr>
-
-                    <tr>
-                        <td><input style="width: 85px;" value="AA190161" type="text"></td>
-                        <td><input style="width: 330px;" value="ARMISHA" type="text"></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td><button class="btn btn-sm btn-success">Add</button></td>
-                    </tr>
+                    $conn->close();
+                    ?>
 
                 </tbody>
             </table>
