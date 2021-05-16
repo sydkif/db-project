04038b6b886da820f093a1c59e9e3770a93ca659
Syed Akif <57567504+sydkif@users.noreply.github.com>
2021-05-16 17:42:11 +0800

    update workload

--- admin/register/lecturer.php
+++ admin/register/lecturer.php
@@ -8,15 +8,15 @@
         </div>
 
         <div class="table-responsive shadow rounded">
-            <table class="table table-striped">
+            <table id="table" class="table table-striped">
                 <thead class="thead-dark">
                     <tr>
                         <th style="width: 10%;">Lecturer ID</th>
                         <th style="width: auto;">Lecturer Name</th>
                         <th style="width: 20%;">Modified By</th>
                         <th style="width: 20%;">Modified On</th>
-                        <th style="width: 5%;">Update</th>
-                        <th style="width: 5%;">Delete</th>
+                        <th style="width: 11%;">Update</th>
+                        <th style="width: 8%;">Delete</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -32,20 +32,29 @@
                         // output data of each row
                         while ($row = $result->fetch_assoc()) {
                             ++$num;
+                            $date = $row['modiOn'];
+                            // date("Y-m-d h:i:sa", $d);
+                            // date_format($date, "Y/m/d H:i:s")
                     ?>
 
                             <tr>
-                                <td id="id<?php echo $num ?>"><b><?php echo $row["id"] ?><b></td>
-                                <td contentEditable=" false" id="name<?php echo $num ?>" style="text-transform: uppercase;"><?php echo $row["name"] ?></td>
-                                <td><?php echo $row["modiBy"] ?></td>
-                                <td><?php echo $row["modiOn"]  ?></td>
+                                <td id="id<?= $num ?>"><b><?= $row["id"] ?><b></td>
+                                <td contentEditable=" false" id="name<?= $num ?>" style="text-transform: uppercase;"><?= $row["name"] ?></td>
+                                <td><?= $row["modiBy"] ?></td>
+                                <td><?= $date ?></td>
                                 <td>
-                                    <button id="update<?php echo $num ?>" onclick="edit(<?php echo $num ?>)" class="btn btn-sm updateBtn">
+                                    <button id="update<?= $num ?>" onclick="edit(<?= $num ?>)" class="btn btn-sm">
                                         <i class="bi bi-pencil-square" style="color: blue;"></i>
+                                    </button>
+                                    <button id="save<?= $num ?>" onclick="update(<?= $num ?>)" class="btn btn-sm">
+                                        <i class="bi bi-save2" style="color: blue;"></i>
+                                    </button>
+                                    <button id="cancel<?= $num ?>" onclick="cancel(<?= $num ?>)" class="btn btn-sm">
+                                        <i class="bi bi-x-square" style="color: gray;"></i>
                                     </button>
                                 </td>
                                 <td>
-                                    <button id="delete<?php echo $num ?>" class="btn btn-sm" onclick="remove(<?php echo $num ?>)">
+                                    <button id="delete<?= $num ?>" class="btn btn-sm" onclick="remove(<?= $num ?>)">
                                         <i class="bi bi-trash" style="color: red;"></i>
                                     </button>
                                 </td>
@@ -61,7 +70,7 @@
 
 
                     <tr>
-                        <form action="" method="post">
+                        <form method="post">
                             <td><input style="width: 60px;" name="id" value="" type="text" minlength="4" maxlength="4" pattern="\d*" required></td>
                             <td><input style="width: 360px;" name="name" value="" type="text" required></td>
                             <td></td>
@@ -78,11 +87,13 @@
                     <?php
 
                     include "../../DB.php";
-
+                    date_default_timezone_set("Asia/Kuala_Lumpur");
                     if (isset($_POST['add'])) {
                         $id = $_POST['id'];
                         $name = $_POST['name'];
-                        $sql = "INSERT INTO lecturer (id, name) VALUES ('$id', '$name')";
+                        $modiBy = "Super Admin"; // TODO Change to session_id later on
+                        $modiOn = date("Y-m-d h:i:s");
+                        $sql = "INSERT INTO lecturer (id, name, modiBy, modiOn) VALUES ('$id', '$name', '$modiBy', '$modiOn')";
 
                         if ($conn->query($sql) === true) {
                             // Success
@@ -102,5 +113,81 @@
         </div>
     </div>
 </div>
+
+<script>
+    var defaultText, t;
+
+    window.onload = function() {
+        var table = document.getElementById("table");
+        var rows = table.getElementsByTagName("tr");
+        for (var x = 1; x <= rows.length; x++) {
+            document.getElementById("save" + x).hidden = true;
+            document.getElementById("cancel" + x).hidden = true;
+        }
+
+    };
+
+
+    function edit(n) {
+        if (defaultText != null) {
+            document.getElementById("name" + t).contentEditable = false;
+            document.getElementById("update" + t).hidden = false;
+            document.getElementById("save" + t).hidden = true;
+            document.getElementById("cancel" + t).hidden = true;
+            document.getElementById("name" + t).style.outlineStyle = "";
+            document.getElementById("name" + t).style.outlineOffset = "";
+            document.getElementById("name" + t).innerText = defaultText;
+        }
+        defaultText = document.getElementById("name" + n).innerText;
+        document.getElementById("name" + n).contentEditable = true;
+        document.getElementById("name" + n).focus();
+        document.getElementById("update" + n).hidden = true;
+        document.getElementById("save" + n).hidden = false;
+        document.getElementById("cancel" + n).hidden = false;
+        document.getElementById("name" + n).style.outlineStyle = "solid";
+        document.getElementById("name" + n).style.outlineOffset = "-10px";
+        t = n;
+    }
+
+    function cancel(n) {
+        // document.getElementById("id" + n).contentEditable = false;
+        document.getElementById("name" + n).contentEditable = false;
+        document.getElementById("update" + n).hidden = false;
+        document.getElementById("save" + n).hidden = true;
+        document.getElementById("cancel" + n).hidden = true;
+        document.getElementById("name" + n).style.outlineStyle = "";
+        document.getElementById("name" + n).style.outlineOffset = "";
+        document.getElementById("name" + n).innerText = defaultText;
+        // location.reload();
+    }
+
+    function update(n) {
+        var id = document.getElementById("id" + n).innerText;
+        var name = document.getElementById("name" + n).innerText;
+        var url = ("../update.php?table=lecturer&id=" + id + "&name=" + name);
+        var msg = "Are you sure want to update this record?";
+        var conf = confirm(msg);
+        if (conf)
+            window.location = "" + url;
+    }
+
+    function remove(n) {
+        var id = document.getElementById("id" + n).innerText;
+        var name = document.getElementById("name" + n).innerText;
+        var url = ("../delete.php?table=lecturer&id=" + id);
+        var msg = "Are you sure want to delete this record?\n\nLecturer ID :\n" + id + "\n\nNAME :\n" + name;
+        var conf = confirm(msg);
+        if (conf)
+            window.location = "" + url;
+    }
+
+    // $('.updateBtn').click(function() {
+    //     $('.updateBtn').not(this).prop('disabled', true);
+    // })
+
+    // $('.cancelBtn').click(function() {
+    //     $('.updateBtn').not(this).prop('disabled', false);
+    // })
+</script>
 
 <?php include '../../templates/footer.php' ?>
--- admin/workload.php
+++ admin/workload.php
@@ -8,14 +8,14 @@
         </div>
 
         <div class="table-responsive shadow rounded">
-            <table class="table table-striped">
+            <table id="table" class="table table-striped">
                 <thead class="thead-dark">
                     <tr>
                         <th style="width: 5%;">No</th>
                         <th style="width: auto;">Lecturer</th>
                         <th style="width: auto;">Subject</th>
-                        <th style="width: 5%;">Update</th>
-                        <th style="width: 5%;">Delete</th>
+                        <th style="width: 11%;">Update</th>
+                        <th style="width: 8%;">Delete</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -24,11 +24,6 @@
                     include "../DB.php";
 
                     $sql = "SELECT lecturer_id, subject_id FROM workload ORDER BY lecturer_id";
-                    // $sql = "SELECT lecturer.name, subject.name 
-                    //         FROM lecturer
-                    //         INNER JOIN subject ON lecturer.subject = subject.id";
-
-                    // "SELECT object.name,formulation.formulation_name as formulation FROM object INNER JOIN formulation ON object.formulation_fk = formulation.id" 
 
                     $result = $conn->query($sql);
                     $num = 0;
@@ -38,22 +33,42 @@
                             ++$num;
                             // $lecturer_name = $row['lecturer_id'];
 
-                            $sql2 = "SELECT * FROM lecturer WHERE id = " . $row['lecturer_id'];
+                            $sql2 = "SELECT name FROM lecturer WHERE id = " . $row['lecturer_id'];
                             $result2 = $conn->query($sql2);
                             $row2 = $result2->fetch_assoc();
 
                             $subject_name = $row['subject_id'];
-                            $sql3 = "SELECT * FROM subject WHERE id = '" . $row['subject_id'] . "'";
+                            $sql3 = "SELECT name FROM subject WHERE id = '" . $row['subject_id'] . "'";
                             $result3 = $conn->query($sql3);
                             $row3 = $result3->fetch_assoc();
                     ?>
 
                             <tr>
-                                <th><?php echo $num ?></th>
-                                <td><?php echo $row2['name'] ?></td>
-                                <td><?php echo $row3['name'] ?></td>
-                                <td><button class="btn btn-sm btn-primary">Update</button></td>
-                                <td><button class="btn btn-sm btn-danger">Delete</button></td>
+                                <form method="post">
+                                    <th><?= $num ?></th>
+                                    <td style="text-transform: uppercase;"><?= $row2['name'] ?>
+                                        <input type="hidden" name="lecturerId" value="<?= $row['lecturer_id'] ?>">
+                                    </td>
+                                    <td style="text-transform: uppercase;"><?= $row3['name'] ?>
+                                        <input type="hidden" name="subjectId" value="<?= $row['subject_id'] ?>">
+                                    </td>
+                                    <td>
+                                        <button id="update<?= $num ?>" onclick="edit(<?= $num ?>)" class="btn btn-sm">
+                                            <i class="bi bi-pencil-square" style="color: blue;"></i>
+                                        </button>
+                                        <button id="save<?= $num ?>" onclick="update(<?= $num ?>)" class="btn btn-sm">
+                                            <i class="bi bi-save2" style="color: blue;"></i>
+                                        </button>
+                                        <button id="cancel<?= $num ?>" onclick="cancel(<?= $num ?>)" class="btn btn-sm">
+                                            <i class="bi bi-x-square" style="color: gray;"></i>
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button name="delete" id="delete<?= $num ?>" class="btn btn-sm" onclick="remove()">
+                                            <i class="bi bi-trash" style="color: red;"></i>
+                                        </button>
+                                    </td>
+                                </form>
                             </tr>
 
                     <?php
@@ -65,65 +80,107 @@
                     ?>
 
 
-
                     <tr>
-                        <th></th>
-                        <td>
-                            <select class="custom-select">
-                                <option selected>Please Choose</option>
+                        <form method="post">
+                            <th></th>
+                            <td>
+                                <select name="lecturer_id" class="custom-select">
+                                    <option selected>Please Choose</option>
 
-                                <?php
-                                include "../DB.php";
+                                    <?php
+                                    include "../DB.php";
 
-                                $sql = "SELECT name FROM lecturer";
-                                $result = $conn->query($sql);
-                                $num = 0;
-                                if ($result->num_rows > 0) {
-                                    // output data of each row
-                                    while ($row = $result->fetch_assoc()) {
-                                        ++$num;
-                                ?>
-                                        <option value="<?php echo $num ?>"><?php echo $row['name'] ?></option>
-                                <?php
+                                    $sql = "SELECT id, name FROM lecturer";
+                                    $result = $conn->query($sql);
+                                    $num = 0;
+                                    if ($result->num_rows > 0) {
+                                        // output data of each row
+                                        while ($row = $result->fetch_assoc()) {
+                                            ++$num;
+                                    ?>
+                                            <option value="<?= $row['id'] ?>"><?= $row['name'] ?></option>
+                                    <?php
+                                        }
+                                    } else {
+                                        echo "0 results";
                                     }
-                                } else {
-                                    echo "0 results";
-                                }
-                                $conn->close();
-                                ?>
+                                    $conn->close();
+                                    ?>
 
-                            </select>
-                        </td>
-                        <td>
-                            <select class="custom-select">
-                                <option selected>Please Choose</option>
+                                </select>
+                            </td>
+                            <td>
+                                <select name="subject_id" class="custom-select">
+                                    <option selected>Please Choose</option>
 
-                                <?php
-                                include "../DB.php";
+                                    <?php
+                                    include "../DB.php";
 
-                                $sql = "SELECT name FROM subject";
-                                $result = $conn->query($sql);
-                                $num = 0;
-                                if ($result->num_rows > 0) {
-                                    // output data of each row
-                                    while ($row = $result->fetch_assoc()) {
-                                        ++$num;
-                                ?>
-                                        <option value="<?php echo $num ?>"><?php echo $row['name'] ?></option>
-                                <?php
+                                    $sql = "SELECT id, name FROM subject";
+                                    $result = $conn->query($sql);
+                                    $num = 0;
+                                    if ($result->num_rows > 0) {
+                                        // output data of each row
+                                        while ($row = $result->fetch_assoc()) {
+                                            ++$num;
+                                    ?>
+                                            <option value="<?= $row['id']  ?>"><?= $row['name'] ?></option>
+                                    <?php
+                                        }
+                                    } else {
+                                        echo "0 results";
                                     }
-                                } else {
-                                    echo "0 results";
-                                }
-                                $conn->close();
-                                ?>
+                                    $conn->close();
+                                    ?>
 
-                            </select>
-                        </td>
-                        <td></td>
-                        <td><button class="btn btn-sm btn-secondary">Add</button></td>
+                                </select>
+                            </td>
+                            <td></td>
+                            <td>
+                                <button name="add" class="btn btn-sm" href="#?">
+                                    <i class="bi bi-plus-square" style="color: green;"></i>
+                                </button>
+                            </td>
+                        </form>
                     </tr>
 
+                    <?php
+
+                    include "../DB.php";
+                    if (isset($_POST['add'])) {
+
+                        $lecturer = $_POST['lecturer_id'];
+                        $subject = $_POST['subject_id'];
+                        $sql = "INSERT INTO workload (lecturer_id, subject_id) VALUES ('$lecturer', '$subject')";
+
+                        if ($conn->query($sql) === true) {
+                            // Success
+                            echo "<meta http-equiv='refresh' content='0'>";
+                        } else {
+                            // Failed
+                            echo "Error: " . $sql . "<br>" . $conn->error;
+                        }
+                    }
+
+                    if (isset($_POST['delete'])) {
+
+                        $lecturer = $_POST['lecturerId'];
+                        $subject =  $_POST['subjectId'];
+
+                        $sql = "DELETE FROM workload WHERE lecturer_id = $lecturer AND subject_id= '" . $subject . "'";
+
+                        if ($conn->query($sql) === true) {
+                            // Success
+                            echo "<meta http-equiv='refresh' content='0'>";
+                        } else {
+                            // Failed
+                            echo "Error: " . $sql . "<br>" . $conn->error;
+                        }
+                    }
+
+                    $conn->close();
+
+                    ?>
 
 
                 </tbody>
@@ -131,5 +188,23 @@
         </div>
     </div>
 </div>
+
+<script>
+    window.onload = function() {
+        var table = document.getElementById("table");
+        var rows = table.getElementsByTagName("tr");
+        for (var x = 1; x <= rows.length; x++) {
+            document.getElementById("save" + x).hidden = true;
+            document.getElementById("cancel" + x).hidden = true;
+        }
+    };
+
+    function remove() {
+        var msg = "Are you sure want to delete this record?";
+        var conf = confirm(msg);
+        if (!conf)
+            window.location = "" + url;
+    }
+</script>
 
 <?php include("../templates/footer.php") ?>
