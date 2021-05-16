78e22cb03c1c3daad572bdd454889ebdfa70ba24
Ameer <ameer_asfa98@hotmail.com>
2021-05-16 12:43:34 +0800

    Added register subject

--- admin/update.php
+++ admin/update.php
@@ -6,13 +6,19 @@ $id = $_GET['id'];
 $table = $_GET['table'];
 $name = $_GET['name'];
 
-$sql = "UPDATE $table SET name='$name' WHERE id=$id";
+$sql = "UPDATE $table SET name='$name' WHERE id="."'".$id."'";
 
 if ($conn->query($sql) === TRUE) {
     echo "Record updated successfully";
     $conn->close();
     if ($table == "admin")
         header("location:register/admin.php");
+    else if ($table == "lecturer")
+        header("location:register/lecturer.php");
+    else if ($table == "student")
+        header("location:register/student.php");
+    else if ($table == "subject")
+        header("location:register/subject.php");
     else
         header("location:/index.php");
 } else {
