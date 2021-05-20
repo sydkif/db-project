cfbc2dfba9e2847364425e9f0b6c6a544f3638d1
aiddin <arcticdessert@gmail.com>
2021-05-20 22:27:36 +0800

    login page
    din's branch

--- index.php
+++ index.php
@@ -4,36 +4,41 @@ $_SESSION['status'] = null;
 $_SESSION['msg'] = null;
 ?>
 
+
 <!-- Login Form START -->
 <div class="container p-5">
-    <h2 style="text-align: center;">Author Wannabe</h2>
+    <h2 style="text-align: center;">Lord Chin CHin</h2>
     <h2 style="text-align: center;">Login Page</h2>
     <hr>
+    
     <div class="col-xl-6 col-lg-6 col col-md-10 col-sm-12 form-box border rounded mt-6 shadow" style="margin:auto; background-color:rgb(246, 246, 246) ;">
-        <form class="mt-3">
-            <div class="form-group">
-                <label for="inputId"><b>ID</b></label>
-                <input type="text" class="form-control" id="inputId" placeholder="Please enter your ID">
-            </div>
-            <div class="form-group">
-                <label for="inputPassword"><b>Password</b></label>
-                <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-            </div>
-            <div class="form-group" style="margin-left: 10px;">
-                <label class="form-check-label"><input type="radio" name="account_type" id="student">
-                    Student</label><br>
-                <label class="form-check-label"><input type="radio" name="account_type" id="lecturer">
-                    Lecturer</label><br>
-                <label class="form-check-label"><input type="radio" name="account_type" id="admin">
-                    Admin</label><br>
-            </div>
-            <button onclick="nextPage()" type="button" class="btn btn-primary btn-block" id="login">Login</button>
+    <form  method="post">
+        <div>
+            <label for="inputId"><b>ID</b></label>
+            <input type="text" class="form-control" name="inputId" placeholder="Please enter your ID" required id="inputId" />
+        </div>
+        <div>
+
+            <label for="inputPassword"><b>Password</b></label>
+            <input type="password" class="form-control" name="txt_password" required>
+
+        </div>
+        <div style="margin-left: 10px;">
+            <label class="form-check-label"><input type="radio" name="account_type" id="student">
+                Chinpira</label><br>
+            <label class="form-check-label"><input type="radio" name="account_type" id="lecturer">
+                Lycra Chromosome</label><br>
+            <label class="form-check-label"><input type="radio" name="account_type" id="admin">
+                Yadaran</label><br>
+        </div>
+        <button href="admin/register/student.php" type="submit" class="btn btn-primary btn-block" id="login">Login</button>
         </form>
+
         <br>
     </div>
 </div>
-<!-- Login Form END -->
 
+<!-- Login Form END -->
 <script>
     function nextPage() {
         if (document.getElementById('student').checked)
@@ -46,5 +51,6 @@ $_SESSION['msg'] = null;
             alert('Please select a user type');
     }
 </script>
+
 
 <?php include('templates/footer.php') ?>
