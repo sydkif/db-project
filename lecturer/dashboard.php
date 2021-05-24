de0077ece41131eca289d7ac1afa1c2e16853a63
Syed Akif <57567504+sydkif@users.noreply.github.com>
2021-05-24 10:31:29 +0800

    Create objective-quiz.php
    - using dummy data with array only
    - add href for add quizs button in lecturer dashboard
    - minor formatting in css

--- lecturer/dashboard.php
+++ lecturer/dashboard.php
@@ -41,13 +41,15 @@
                                 </td>
                                 <td>
                                     <div style="display:flex; align-items:center; justify-content:center;">
-                                        <button class="btn btn-sm" title="Add New True False Quiz" style="margin-right: 10px;"><i class="bi bi-clipboard-plus" style="font-size: 28px;"></i></button>
+                                        <button class="btn btn-sm" title="Add New True False Quiz" style="margin-right: 10px;" onclick="location.href = 'create/true-false-quiz.php';">
+                                            <i class=" bi bi-clipboard-plus" style="font-size: 28px; color:forestgreen;"></i></button>
                                         <button class="btn btn-sm" title="View True False Result"><i class="bi bi-clipboard-data" style="font-size: 28px;"></i></button>
                                     </div>
                                 </td>
                                 <td>
                                     <div style="display:flex; align-items:center; justify-content:center;">
-                                        <button class="btn btn-sm" title="Add New Objective Quiz" style="margin-right: 10px;"><i class="bi bi-clipboard-plus" style="font-size: 28px;"></i></button>
+                                        <button class="btn btn-sm" title="Add New Objective Quiz" style="margin-right: 10px;" onclick="location.href = 'create/objective-quiz.php';">
+                                            <i class=" bi bi-clipboard-plus" style="font-size: 28px; color:forestgreen;"></i></button>
                                         <button class="btn btn-sm" title="View Objective Result"><i class=" bi bi-clipboard-data" style="font-size: 28px;"></i></button>
                                     </div>
                                 </td>
