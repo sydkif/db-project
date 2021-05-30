5fbc7094b866ad3cf435cda7d8ab8026dfe35569
aiddin <arcticdessert@gmail.com>
2021-05-30 17:20:57 +0800

    Test

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $_SESSION['msg'] = null;
     <hr>
     
     <div class="col-xl-6 col-lg-6 col col-md-10 col-sm-12 form-box border rounded mt-6 shadow" style="margin:auto; background-color:rgb(246, 246, 246) ;">
+<<<<<<< Updated upstream
     <form  action="admin/dashboard.php">
         <div>
             <label for="inputId"><b>ID</b></label>
@@ -23,10 +24,71 @@ $_SESSION['msg'] = null;
         </div>
         <br>
         <button input type="submit" class="btn btn-primary btn-block" id="login">Login</button>
+=======
+        <form class="mt-3" name="login" action= "login.php" onsubmit = "return validation()" method = "POST" class="needs-validation" novalidate="" autocomplete="on">
+            <div class="form-group">
+                <label for="inputId"><b>ID</b></label>
+                <input type="text" class="form-control" id="inputId" placeholder="Please enter your ID">
+            </div>
+            <div class="form-group">
+                <label for="inputPassword"><b>Password</b></label>
+                <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+            </div>
+            <div class="form-group" style="margin-left: 10px;">
+                <label class="form-check-label"><input type="radio" name="account_type" id="student">
+                    Student</label><br>
+                <label class="form-check-label"><input type="radio" name="account_type" id="lecturer">
+                    Lecturer</label><br>
+                <label class="form-check-label"><input type="radio" name="account_type" id="admin">
+                    Admin</label><br>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block login-btn" id="login"><span>Login</span></button>
+>>>>>>> Stashed changes
         </form>
         <a class="text-align: center;" href ="admin/login/studentLogin.php">Student</a>
 
         <br>
     </div>
 </div>
+<<<<<<< Updated upstream
+=======
+<!-- Login Form END -->
+
+<script>
+    function nextPage() {
+        if (document.getElementById('student').checked)
+            window.location.href = "student/dashboard.php";
+        else if (document.getElementById('lecturer').checked)
+            window.location.href = "lecturer/dashboard.php";
+        else if (document.getElementById('admin').checked)
+            window.location.href = "admin/dashboard.php";
+        else
+            alert('Please select a user type');
+    }
+</script>
+<script>
+         function validation()  
+            {  
+                var id=document.login.user.value;  
+                var ps=document.login.pass.value;  
+                if(id.length=="" && ps.length=="") {  
+                    alert("User Name and Password fields are empty");  
+                    return false;  
+                }  
+                else  
+                {  
+                    if(id.length=="") {  
+                        alert("User Name is empty");  
+                        return false;  
+                    }   
+                    if (ps.length=="") {  
+                    alert("Password field is empty");  
+                    return false;  
+                    }  
+                }                             
+            }  
+	
+
+</script>
+>>>>>>> Stashed changes
 <?php include('templates/footer.php') ?>
