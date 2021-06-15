96ebb579db41d682f7c753f2bb8a916093074fbc
Syed Akif <57567504+sydkif@users.noreply.github.com>
2021-06-15 13:01:47 +0800

    Minor update admin module
    - Move [+] button  to out of table

--- admin/register/student.php
+++ admin/register/student.php
@@ -65,91 +65,86 @@
                     $conn->close();
 
                     ?>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td style="text-align: center;">
-                            <button type="button" class="btn btn-sm" data-toggle="modal" data-target="#staticBackdrop">
-                                <i class="bi bi-plus-square" style="color: green;"></i>
-                            </button>
-
-                            <!-- Modal -->
-                            <div style="text-align: left;">
-                                <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                    <div class="modal-dialog modal-dialog-centered">
-                                        <div class="modal-content">
-                                            <form method="POST">
-                                                <div class="modal-header">
-
-                                                    <h5 class="modal-title" id="staticBackdropLabel">Register Student</h5>
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                        <span aria-hidden="true">&times;</span>
-                                                    </button>
-                                                </div>
-
-                                                <div class="modal-body">
-
-                                                    <div class="form-group">
-                                                        <label class="col-form-label"><b>Student ID</b></label>
-                                                        <input class="form-control" id="question" name="id" required>
-                                                        <label class="col-form-label"><b>Student Name</b></label>
-                                                        <input class="form-control" id="question" name="name" required>
-                                                    </div>
-                                                </div>
-
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                    <button type="submit" class="btn btn-primary" name="add">Add</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </td>
-                    </tr>
-                    <?php
-
-                    include "../../database/DB.php";
-                    date_default_timezone_set("Asia/Kuala_Lumpur");
-                    if (isset($_POST['add'])) {
-                        $id = $_POST['id'];
-                        $id = strtoupper($id);
-                        $name = strtoupper($_POST['name']);
-                        $email = $id . "@siswa.uthm.edu.my";
-                        $modiBy = "Super admin";
-                        $modiOn = date("Y-m-d H:i:s");
-                        $sql = "INSERT INTO student (id, name, email, password, modiBy, modiOn) VALUES('$id', '$name', '$email', '$id', '$modiBy', '$modiOn')";
-
-                        if ($conn->query($sql) === true) {
-                            // Success
-                            $_SESSION['msg'] = "Record added successfully!";
-                            $_SESSION['status'] = "Success";
-                        } else {
-                            // Failed
-                            $_SESSION['msg'] = "Error: " . $sql . " | " . $conn->error;
-                            $_SESSION['status'] = "Fail";
-                        }
-                        echo "<meta http-equiv='refresh' content='0'>";
-                    }
-
-                    $conn->close();
-
-                    ?>
-
-
-
 
                 </tbody>
             </table>
 
         </div>
     </div>
+    <br>
+    <div style="text-align: right;">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
+                <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"></path>
+            </svg>
+            New Student
+        </button>
+
+        <!-- Modal -->
+        <div style="text-align: left;">
+            <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <form method="POST">
+                            <div class="modal-header">
+
+                                <h5 class="modal-title" id="staticBackdropLabel">Please enter Student ID and Name</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+
+                            <div class="modal-body">
+
+                                <div class="form-group">
+                                    <label class="col-form-label"><b>Student ID</b></label>
+                                    <input class="form-control" id="question" name="id" required>
+                                    <label class="col-form-label"><b>Student Name</b></label>
+                                    <input class="form-control" id="question" name="name" required>
+                                </div>
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary" name="add">Add</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+
+    include "../../database/DB.php";
+    date_default_timezone_set("Asia/Kuala_Lumpur");
+    if (isset($_POST['add'])) {
+        $id = $_POST['id'];
+        $id = strtoupper($id);
+        $name = strtoupper($_POST['name']);
+        $email = $id . "@siswa.uthm.edu.my";
+        $modiBy = $_SESSION['usersname'];
+        $modiOn = date("Y-m-d H:i:s");
+        $sql = "INSERT INTO student (id, name, email, password, modiBy, modiOn) VALUES('$id', '$name', '$email', '$id', '$modiBy', '$modiOn')";
+
+        if ($conn->query($sql) === true) {
+            // Success
+            $_SESSION['msg'] = "Record added successfully!";
+            $_SESSION['status'] = "Success";
+        } else {
+            // Failed
+            $_SESSION['msg'] = "Error: " . $sql . " | " . $conn->error;
+            $_SESSION['status'] = "Fail";
+        }
+        echo "<meta http-equiv='refresh' content='0'>";
+    }
+
+    $conn->close();
+
+    ?>
+
 </div>
 
 <?php include('../../templates/footer.php') ?>
